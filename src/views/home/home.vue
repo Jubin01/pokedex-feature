6ff2a82096cd0a97003738e41909a531e427e39e
Jubin01 <jubinmail@gmail.com>
2024-09-18 14:01:10 +0530

    #change svg to png pokemon images (#1)
    Co-authored-by: jubin thomas <athom130@adient.com>

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,12 +2,13 @@
   <div class="home">
     <h3>Home</h3>
 
-    <div class="center" v-for="(value, index) in pokemon" :key="index">
-      <a>
-        <img :src=value.sprites.other.dream_world.front_default height="100" />
-      </a>
-      <h5>{{ value.name.charAt(0).toUpperCase() + value.name.slice(1) }}</h5>
-      <a  class= "badge" v-for="(value, index) in value.types" :key="index">{{ value.type.name}}</a>
+    <div class="center">
+      <img v-if="pokemon.sprites" id="pokeimage" :src=pokemon.sprites.front_default height="100" />
+      <!-- <h5>{{ value.name.charAt(0).toUpperCase() + value.name.slice(1) }}</h5> -->
+      <div>
+        <h5>{{ pokemon.name }}</h5>
+      </div>
+      <span class="badge" v-for="(value, index) in pokemon.types" :key="index">{{ value.type.name }}</span>
       <br />
       <a v-on:click="onPrevClick" href="#" class="previous">&laquo; Previous</a>
       <a v-on:click="onNextClick" href="#" class="next">Next &raquo;</a>
@@ -18,10 +19,9 @@
   
 <style>
 a {
-  text-decoration: none;
-  display: inline-block;
   padding: 8px 16px;
 }
+
 .badge {
   background-color: red;
   color: white;
@@ -40,23 +40,24 @@ export default {
   name: 'Home',
   data: () => ({
     i: 1,
-    pokemon: []
+    pokemon: {}
   }),
   methods: {
-    onPrevClick: function () {
-      if(this.i > 1){
-        this.callPokemon(this.i-=1);
-      } 
+    onPrevClick() {
+      if (this.i > 1) {
+        this.callPokemon(this.i -= 1);
+      }
     },
-    onNextClick: function () {
-      this.callPokemon(this.i+=1);
+    onNextClick() {
+      this.callPokemon(this.i += 1);
     },
-    callPokemon: function (i) {
+    callPokemon(i) {
       const xhr = new XMLHttpRequest();
       xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + i + '/');
       xhr.onload = () => {
-        this.pokemon = [];
-        this.pokemon.push(JSON.parse(xhr.responseText));
+        this.pokemon = {};
+        this.pokemon = JSON.parse(xhr.responseText);
+        // document.getElementById("pokeimage").src = this.pokemon.sprites.front_default;
       }
       xhr.send();
     }
